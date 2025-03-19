d80b86b1f91f3f5411a8aa7ed06e0371047f2725
mumumu <takumu.sato@gmail.com>
2025-03-19 15:21:22 +0900

    fix : secret.envの読み込みの設定を追加

--- src/common/ENV.ts
+++ src/common/ENV.ts
@@ -1,11 +1,14 @@
 import jetEnv, { num, str } from "jet-env";
 import { isEnumVal } from "jet-validators";
+import dotenv from "dotenv";
 
 import { NodeEnvs } from "./constants";
 
 /******************************************************************************
                                  Setup
 ******************************************************************************/
+
+const secretEnv = dotenv.config({ path: "./config/secret.env" });
 
 const ENV = jetEnv({
   NodeEnv: isEnumVal(NodeEnvs),
