681ad24c5d4bf26cd87c747251b679ea77be362f
mumumu <takumu.sato@gmail.com>
2025-03-19 11:33:59 +0900

    fix : nodeenvに環境変数の設定を追加

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,11 @@
-
 /******************************************************************************
                               Enums
 ******************************************************************************/
 
 // NOTE: These need to match the names of your ".env" files
 export enum NodeEnvs {
-  Dev = 'development',
-  Test = 'test',
-  Production = 'production'
+  Dev = "development",
+  Test = "test",
+  Staging = "staging",
+  Production = "production",
 }
